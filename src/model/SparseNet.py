3359588e60e155ebc62e19225dc651dea6b8d37c
lpjiang97 <prestonj@cs.washington.edu>
2020-02-20 15:42:41 -0800

    sparse code inf threshold

--- src/model/SparseNet.py
+++ src/model/SparseNet.py
@@ -5,17 +5,18 @@ import torch.nn.functional as F
 
 class SparseNet(nn.Module):
 
-    def __init__(self, K:int, M:int, R_lr:float=0.1, lmda:float=5e-3):
+    def __init__(self, K:int, M:int, R_lr:float=0.1, lmda:float=5e-3, device=None):
         super(SparseNet, self).__init__()
         self.K = K
         self.M = M
         self.R_lr = R_lr
         self.lmda = lmda
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # synaptic weights
-        self.U = nn.Linear(self.M ** 2, self.K, bias=False).to(self.device)
+        self.device = torch.device("cpu") if device is None else device
+        self.U = nn.Linear(self.K, self.M ** 2, bias=False).to(self.device)
         # responses
         self.R = None
+        self.normalize_weights()
 
     def ista_(self, img_batch):
         # create R
@@ -36,7 +37,7 @@ class SparseNet(nn.Module):
             # zero grad
             self.zero_grad()
             # prox
-            self.R = SparseNet.soft_thresholding_(self.R, self.lmda)
+            self.R.data = SparseNet.soft_thresholding_(self.R, self.lmda)
             # convergence
             converged = torch.norm(self.R - old_R) / torch.norm(old_R) < 0.01
 
@@ -44,11 +45,15 @@ class SparseNet(nn.Module):
     def soft_thresholding_(x, alpha):
         with torch.no_grad():
             rtn = F.relu(x - alpha) - F.relu(-x - alpha)
-        return rtn
+        return rtn.data
 
     def zero_grad(self):
-        self.R.grad.data.zero_()
+        self.R.grad.zero_()
         self.U.zero_grad()
+
+    def normalize_weights(self):
+        with torch.no_grad():
+            self.U.weight.data = F.normalize(self.U.weight.data, dim=0)
 
     def forward(self, img_batch):
         # first fit
