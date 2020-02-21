eaf407daee50378bf3de57cbb75b121b4e71a0ca
lpjiang97 <prestonj@cs.washington.edu>
2020-02-20 18:50:23 -0800

    update plotting code

--- src/scripts/plotting.py
+++ src/scripts/plotting.py
@@ -1,25 +1,28 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../.'))
-import torch
+import numpy as np
 import matplotlib.pyplot as plt
-from src.utils.cmd_line import parse_args
 
 
-### choose the model to load ###
-arg = parse_args()
-
-# load model
-model = torch.load(f"../../trained_models/model_epoch-{arg.epoch}_N-{arg.batch_size}_K-{arg.n_neuron}_M-{arg.size}_lmda-{arg.reg}_Rlr_{arg.r_learning_rate}_Ulr_{arg.learning_rate}.pth")
-fig, axes = plt.subplots(nrows=10, ncols=10, sharex=True, sharey=True)
-# get receptive fields
-U = model.U.clone()
-if U.is_cuda:
-    U = U.cpu()
-U = U.data.numpy()
-for i in range(10):
-    for j in range(10):
-        ax = axes[i, j]
-        ax.imshow(U[i * 10 + j, :].reshape(-1,10), cmap='gray')
-fig.set_size_inches(15, 15)
-fig.savefig(f"../../trained_models/model_epoch-{arg.epoch}_N-{arg.batch_size}_K-{arg.n_neuron}_M-{arg.size}_lmda-{arg.reg}_Rlr_{arg.r_learning_rate}_Ulr_{arg.learning_rate}.png", dpi=300)
+def plot_rf(rf, out_dim, M):
+    rf = rf.reshape(out_dim, -1)
+    # normalize
+    rf = rf.T / np.abs(rf).max(axis=1)
+    rf = rf.T
+    rf = rf.reshape(out_dim, M, M)
+    # plotting
+    n = int(np.ceil(np.sqrt(rf.shape[0])))
+    fig, axes = plt.subplots(nrows=n, ncols=n, sharex=True, sharey=True)
+    fig.set_size_inches(10, 10)
+    for i in range(rf.shape[0]):
+        ax = axes[i // n][i % n]
+        ax.imshow(rf[i], cmap='gray', vmin=-1, vmax=1)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_aspect('equal')
+    for j in range(rf.shape[0], n * n):
+        ax = axes[j // n][j % n]
+        ax.imshow(np.ones_like(rf[0]) * -1, cmap='gray', vmin=-1, vmax=1)
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_aspect('equal')
+    fig.subplots_adjust(wspace=0.0, hspace=0.0)
+    return fig
